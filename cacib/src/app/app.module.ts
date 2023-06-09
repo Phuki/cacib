13b2a039831c3de1b9daf0cbac51846db167a13b
Phuki <truongphuquy3@gmail.com>
2023-06-09 19:52:34 +0200

    feat(navbar): add navbar

---
+++ cacib/src/app/app.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import {NavbarComponent} from "./core/navbar/navbar.component";
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    NavbarComponent
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
